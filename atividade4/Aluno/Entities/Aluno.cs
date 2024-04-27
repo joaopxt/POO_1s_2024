07698fba59e6e2c1521bb01a475513263c05ff3c
Joao Peixoto <peixoto.jvs@gmail.com>
2024-04-27 10:21:57 -0300

    Att Atividade4

--- atividade4/Aluno/Entities/Aluno.cs
+++ atividade4/Aluno/Entities/Aluno.cs
@@ -20,6 +20,61 @@ public class Aluno { // Classe de Aluno com métodos de Update e Delete
         Semestre = semestre;
     }
 
+    public Atleta atletaAtr(){
+
+        System.Console.WriteLine("Nome: ");
+        string nome = Console.ReadLine();
+        System.Console.WriteLine("Curso: ");
+        string curso = Console.ReadLine();
+        System.Console.WriteLine("Semestre: ");
+        int sem = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Esporte: ");
+        string esp = Console.ReadLine();
+        System.Console.WriteLine("Tecnico: ");
+        string tec = Console.ReadLine();
+        System.Console.WriteLine("Bolsa: ");
+        double bol = double.Parse(Console.ReadLine());
+
+          Atleta a1 = new Atleta(nome, curso, sem, esp, bol, tec);
+         return a1;
+    }
+
+    public Intercambista interAtr( ){
+
+        System.Console.WriteLine("Nome: ");
+        string nome = Console.ReadLine();
+        System.Console.WriteLine("Curso: ");
+        string curso = Console.ReadLine();
+        System.Console.WriteLine("Semestre: ");
+        int sem = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Pais de origem: ");
+        string pais = Console.ReadLine();
+        System.Console.WriteLine("Universidade de origem: ");
+        string uni = Console.ReadLine();
+
+          Intercambista a1 = new Intercambista(nome, curso, sem, pais, uni);
+         return a1;
+    }
+
+    public A_Monitor monAtr(){
+
+        System.Console.WriteLine("Nome: ");
+        string nome = Console.ReadLine();
+        System.Console.WriteLine("Curso: ");
+        string curso = Console.ReadLine();
+        System.Console.WriteLine("Semestre: ");
+        int sem = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Materia Monitorada: ");
+        string mat = Console.ReadLine();
+        System.Console.WriteLine("Bolsa: ");
+        double bol = double.Parse(Console.ReadLine());
+        System.Console.WriteLine("Orientador: ");
+        string orientador = Console.ReadLine();
+
+          A_Monitor a1 = new A_Monitor(nome, curso, sem, mat, bol, orientador);
+         return a1;
+    }
+
     public override string ToString() {
         return $"Aluno #{Id}: {Nome}, {Curso}, {Semestre}";
     }
--- atividade4/Aluno/Program.cs
+++ atividade4/Aluno/Program.cs
@@ -2,6 +2,32 @@
 using Estudante.Entities;
 using System.IO;
 
+Aluno al = new Aluno();
+
+System.Console.WriteLine("Escolha a sua atribuicao de aluno: ");
+string tipo = Console.ReadLine();
+
+switch (tipo){
+    case "Atleta":
+      System.Console.WriteLine(al.atletaAtr());
+    break;
+
+    case "Intercambista":
+    System.Console.WriteLine(al.interAtr());
+    break;
+
+    case "Monitor":
+    System.Console.WriteLine(al.monAtr());
+    break;
+     
+     default:
+     System.Console.WriteLine("Atribuicao nao encontrada!");
+     break;
+}
+
+System.Console.WriteLine();
+
+/*
 Aluno al1 = new Aluno("Joao", "Engenharia de Software", 5);
 A_Monitor mon1 = new A_Monitor("Pedro", "Ciencia da Computacao", 6, "POO", 500.00, "Joao Pedro");
 Atleta atl1 = new Atleta("Cris", "Enfermagem", 8, "Tenis", 300.00, "Melina");
@@ -10,5 +36,5 @@ Intercambista int1 = new Intercambista("Tyler", "Musica", 3, "EUA", "Duke");
 Console.WriteLine(al1);
 Console.WriteLine(mon1);
 Console.WriteLine(atl1);
-Console.WriteLine(int1);
+Console.WriteLine(int1);*/
 
